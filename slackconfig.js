fc033cf5d7f18bbbd61b52b03a978f6489df326d
kbeesley-dev <kbeesley.dev@gmail.com>
2015-09-21 13:08:43 -0500

    handling config vars

--- slackconfig.js
+++ slackconfig.js
@@ -1,4 +1,22 @@
+function prepareEnvVar(envVar)
+{
+    var ret = envVar;
+    
+    console.log('prepareEnvVar');
+    console.log(ret);
+    if(ret.charAt(0) === "'" && ret.charAt(ret.length - 1) === "'")
+    {
+        ret = ret.substr(1,envVar.length - 2);
+        console.log(ret);
+    }
+    
+    ret = JSON.parse(ret);
+    console.log(ret);
+    
+    return ret;
+}
+
 module.exports = {
-    postingSlackWebHook: JSON.parse(process.env.SLACK_POSTINGWEBHOOK),
-    incomingSlackToken: JSON.parse(process.env.SLACK_INCOMINGTOKEN)
+    postingSlackWebHook: prepareEnvVar(process.env.SLACK_POSTINGWEBHOOK),
+    incomingSlackToken: prepareEnvVar(process.env.SLACK_INCOMINGTOKEN)
 };
